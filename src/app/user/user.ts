dd441c9f7908eebe3b986b8a0d328c5ca3a72342
aroharo <wfgggghhjh@gmail.com>
2025-12-10 12:49:17 +0100

    Added random user feature

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
-    templateUrl: './user.html',
+  templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
-
+  selectedUser = DUMMY_USERS[randomIndex];
 }
